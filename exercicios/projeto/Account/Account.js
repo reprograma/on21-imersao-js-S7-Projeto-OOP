3d56929ff53027512204741448dac10fe862dc91
Ana Paula <anapaula.lange@gmail.com>
2022-11-03 20:32:03 -0300

    Atividade da Semana 7

--- exercicios/projeto/Account/Account.js
+++ exercicios/projeto/Account/Account.js
@@ -10,6 +10,28 @@ class Account {
     this.#balance = balance;
   }
 
+  constructor(accountNumber, agency, balance){
+    this.#accountNumber = accountNumber;
+    this.#agency = agency;
+    this.#balance = balance;
+    this.pixKeys = {
+      cpf: undefined,
+      email: undefined,
+      telefone: undefined,
+    }
+  }
+
+  createAccount(accountNumber, agency, balance){
+    if(accountNumber.length === 5 && agency.length === 4 && balance > 0){
+      this.#accountNumber = accountNumber;
+      this.#agency = agency;
+      this.#balance = balance;
+      return "Conta criada com sucesso!"
+    } else {
+      throw new Error("Dados inválidos");
+    }
+  }
+
   getBalance() {
     return this.#balance;
   }
@@ -23,6 +45,16 @@ class Account {
     } else {
       throw new Error("Não é possível depositar valores negativos");
     }
+  }
+}
+
+transferTo(anotherAccount, value) {
+  if (this.balance >= value) {
+    const newValue = this.balance - value;
+    this.balance = newValue;
+    anotherAccount.deposit(value);
+  } else {
+    throw new Error("Saldo insuficiente");
   }
 }
 
