5095c448da8f6e412ed2ddce9c0da907d8a292b0
xeniabarreto <xeniabarreto22@gmail.com>
2022-11-03 00:41:21 -0300

    Add tests of Client

--- exercicios/projeto/Client/Client.test.js
+++ exercicios/projeto/Client/Client.test.js
@@ -1,22 +1,44 @@
-import Account from '../Account/Account.js';
-import Client from './Client.js';
+import Account from "../Account/Account.js";
+import Client from "./Client.js";
+
+const client = new Client();
+const account = new Account();
 
 describe("Teste da classe Client", () => {
   test("verificar se instancia do Client é feita corretamente", () => {
-    const client = new Client();
     // instanciaASerVerificada instanceof Classe -> true ou false
     // alternativa: expect(client).toBeInstanceOf(Client);
     expect(client instanceof Client).toBe(true);
   });
 
   test("cadastrar cliente com dados válidos", () => {
-    const client = new Client();
-    const account = new Account();
-    expect(client.registerClient("Ana", "1234567908", account, 5000)).toBe("Cliente cadastrado");
+    expect(client.registerClient("Ana", "1234567908", account, 5000)).toBe(
+      "Cliente cadastrado"
+    );
   });
 
   test("cadastrar cliente com dados inválidos", () => {
-    const client = new Client();
-    expect(() => client.registerClient("Ana", "1234567908", "não conta", 5000)).toThrow("Erro no cadastro, dados inválidos");
+    expect(() =>
+      client.registerClient("Ana", "1234567908", "não conta", 5000)
+    ).toThrow("Erro no cadastro, dados inválidos");
   });
+
+  test("regsitrar categoria STANDARD e limite R$1000,00", () => {
+    client.registerClient("Ana", "1234567908", account, 3000);
+    expect(client.typeAccount).toBe("STANDARD");
+    expect(client.transactionLimit).toBe(1000);
+  });
+
+  test("regsitrar categoria GOLD e limite R$5000,00", () => {
+    client.registerClient("Ana", "1234567908", account, 6000);
+    expect(client.typeAccount).toBe("GOLD");
+    expect(client.transactionLimit).toBe(5000);
+  });
+
+  test("regsitrar categoria PREMIUM sem limite de transação", () => {
+    client.registerClient("Ana", "1234567908", account, 19000);
+    expect(client.typeAccount).toBe("PREMIUM");
+  });
+
+
 });
