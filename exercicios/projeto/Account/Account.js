3b5943113388f24c9a87fb54a5be3826de134569
Ana Paula <anapaula.lange@gmail.com>
2022-11-04 15:33:46 -0300

    ajuste das validacoes

--- exercicios/projeto/Account/Account.js
+++ exercicios/projeto/Account/Account.js
@@ -48,14 +48,41 @@ class Account {
   }
 }
 
-transferTo(anotherAccount, value) {
-  if (this.balance >= value) {
-    const newValue = this.balance - value;
-    this.balance = newValue;
-    anotherAccount.deposit(value);
-  } else {
-    throw new Error("Saldo insuficiente");
+createPixKey(keyValue, keyType){
+  switch(keyType){
+      case "CPF":
+          let regex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
+          if(regex.test(keyValue)){
+              this.pixKeys.cpf = keyValue;
+              return "Chave pix cpf criada com sucesso";
+          }else {
+              throw new Error("Erro CPF inválido");
+          }
+      case "EMAIL":
+          this.pixKeys.email = keyValue;
+          return "Chave pix email criada com sucesso";
+      case "TELEFONE":
+          this.pixKeys.telefone = keyValue;
+          return "Chave pix telefone criada com sucesso";
+      default:
+          return "Tipo de chave inexistente"
   }
 }
+
+// corrigir antes de subir:
+// transferTo(cpf, othercount, valor) {
+//   if (this.cpf === cpf) {
+//       this.verificarValor(valor);
+//       if (this.#balance >= valor) {
+//           this.#balance -= valor;
+//           othercount.deposito(valor);
+//           return "Transferência realizada com sucesso!"
+//       } else {
+//           throw new Error("Saldo insuficiente!");
+//       }
+//   } else {
+//       throw Error('Cpf inválido!')
+//   }
+// }
 
 export default Account;
