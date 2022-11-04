aca127a24145346cb4df4c8b20e8a7587cecf078
Renata Igarashi <renata.sayuri@dasa.com.br>
2022-11-04 00:12:50 -0300

    fix tests

--- exercicios/projeto/Client/Client.test.js
+++ exercicios/projeto/Client/Client.test.js
@@ -18,30 +18,3 @@ describe('Client', () => {
     expect(() => client.registerClient('Renata', '111222333-66', null, 7000)).toThrow("Registration error. Invalid data.")
   })
 })
-// import { Account } from '../Account/Account.js'
-// import { Client } from './Client.js'
-
-// describe('Client', () => {
-//     test('Check if the client instance is done correctly', () => {
-//         const client = new Client()
-//         expect(client instanceof Client).toBe(true)
-//     })
-
-//     test('register client with valid data', () => {
-//         const account = new Account()
-//         const client = new Client();
-//         console.log(client);
-//         expect(client.registerClient('Renata', '11122233344', account, 5000)).toBe('Client registered successfully')
-        
-//     })
-
-//     test('try to register client with invalid data', () => {
-//         try {
-//             const client = new Client();
-//             expect(client.registerClient('Renata', '123456789', 'not account', 5000))
-//           } catch (error) {
-//             expect(error.message).toEqual('Registration error: invalid data')
-//           }
-//         })
-    
-// })
