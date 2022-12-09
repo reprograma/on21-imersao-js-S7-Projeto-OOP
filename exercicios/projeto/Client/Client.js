9f343981ce2b192e64366b9b94cd68893f69b98e
Silvovisk <vascsilvia@gmail.com>
2022-12-09 10:58:39 -0300

    projeto

--- exercicios/projeto/Client/Client.js
+++ exercicios/projeto/Client/Client.js
@@ -1,15 +1,53 @@
+import Account from "../Account/Account.js";
+
 class Client {
-    name;
-    #cpf;
-    #account;
-    #income;
-   
-    constructor(name, cpf, account, income){
-       this.name = name;
-       this.#cpf = cpf;
-       this.#account = account;
-       this.#income = income;
+  name;
+  #cpf;
+  #account;
+  #income;
+  typeAccount;
+  transactionLimit;
+
+  // constructor(name, cpf, account, income) {
+  //   this.name = name;
+  //   this.#cpf = cpf;
+  //   this.#account = account;
+  //   this.#income = income;
+  // }
+
+
+  registerClient(name, cpf, account, income) {
+    if (account instanceof Account) {
+      this.name = name;
+      this.#cpf = cpf;
+      this.#account = account;
+      this.#income = income;
+
+      if(this.#income > 0 && this.#income <= 4999.99){
+        this.typeAccount = "STANDARD"
+        this.transactionLimit = 1000
+      } else if (this.#income > 5000 && this.#income <= 17999.99){
+        this.typeAccount = "GOLD"
+        this.transactionLimit = 5000
+      } else if (this.#income >= 18000){
+        this.typeAccount = "PREMIUM"
+        this.transactionLimit = 999999999.99
+      }
+
+      return "Cliente cadastrado";
+    } else {
+      throw new Error("Erro no cadastro, dados inválidos");
     }
-   }
-   
-   export default Client;
+  }
+
+  get typeAccount(){
+    return this.typeAccount
+  }
+
+  get transactionLimit(){
+    return this.transactionLimit
+  }
+
+}
+
+export default Client
--- exercicios/projeto/Client/Client.test.js
+++ exercicios/projeto/Client/Client.test.js
@@ -1,9 +1,44 @@
-import Client from ('./Client');
+import Account from "../Account/Account.js";
+import Client from "./Client.js";
 
-describe("Teste da classe Cliente", () => {
-    
-    it("deve verificar se instância do Cliente é feita corretamente", () =>{
-        const client = new Client();
-        expect()
-    })
-})
+const client = new Client();
+const account = new Account();
+
+describe("Teste da classe Client", () => {
+  test("verificar se instancia do Client é feita corretamente", () => {
+    // instanciaASerVerificada instanceof Classe -> true ou false
+    // alternativa: expect(client).toBeInstanceOf(Client);
+    expect(client instanceof Client).toBe(true);
+  });
+
+  test("cadastrar cliente com dados válidos", () => {
+    expect(client.registerClient("Ana", "1234567908", account, 5000)).toBe(
+      "Cliente cadastrado"
+    );
+  });
+
+  test("cadastrar cliente com dados inválidos", () => {
+    expect(() =>
+      client.registerClient("Ana", "1234567908", "não conta", 5000)
+    ).toThrow("Erro no cadastro, dados inválidos");
+  });
+
+  test("regsitrar categoria STANDARD e limite R$1000,00", () => {
+    client.registerClient("Ana", "1234567908", account, 3000);
+    expect(client.typeAccount).toBe("STANDARD");
+    expect(client.transactionLimit).toBe(1000);
+  });
+
+  test("regsitrar categoria GOLD e limite R$5000,00", () => {
+    client.registerClient("Ana", "1234567908", account, 6000);
+    expect(client.typeAccount).toBe("GOLD");
+    expect(client.transactionLimit).toBe(5000);
+  });
+
+  test("regsitrar categoria PREMIUM sem limite de transação", () => {
+    client.registerClient("Ana", "1234567908", account, 19000);
+    expect(client.typeAccount).toBe("PREMIUM");
+  });
+
+
+});
